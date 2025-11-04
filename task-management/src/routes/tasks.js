5f911538a258c299a5aa5ce82a1ab1fba6a5f877
sakib276 <nazmussakib06927@gmail.com>
2025-11-04 22:53:42 +0600

    lab-01 assignment-06, Add error handling

--- task-management/src/routes/tasks.js
+++ task-management/src/routes/tasks.js
@@ -13,11 +13,16 @@ router.get('/', (req, res) => res.json(tasks));
 
 router.get('/:id', (req, res) => {
   const id = parseInt(req.params.id);
+
+
   if (isNaN(id)) {
     return res.status(400).json({ error: 'Invalid ID format' });
   }
 
+  
   const task = tasks.find(t => t.id === id);
+
+ 
   if (!task) {
     return res.status(404).json({ error: 'Task not found' });
   }
